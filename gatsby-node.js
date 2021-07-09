7de59d73a9c5cbe93d91f2c2129f3a96f9dc3b8f
Jon Best <smile@jonbest.ca>
2021-07-09 11:48:12 -0600

    hacking my way to a SPA

--- gatsby-node.js
+++ gatsby-node.js
@@ -10,8 +10,16 @@ exports.onCreatePage = ({ page, actions }) => {
   const { createPage } = actions;
 
   // page.matchPath is a special key that's used for matching pages only on the client
-  if (page.path === `/`) {
+
+  /*if (page.path === `/`) {
     page.matchPath = `/*`;
+
+    // Update the page.
+    createPage(page);
+  }*/
+
+  if (page.path.match(/^\/jukebox-app/)) {
+    page.matchPath = "/jukebox-app/*"
 
     // Update the page.
     createPage(page);
